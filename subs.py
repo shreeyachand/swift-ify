140867f8e4bb2615d985296ff7379b856bb40ca9
Shreeya Chand <chshreeya@gmail.com>
2021-11-14 16:49:39 -0500

    Includes Red tv

--- subs.py
+++ subs.py
@@ -24,8 +24,33 @@ taylors_version_equivalents = {
     '0xvsgzM8AtBtRHZm5rav8A':'1cSFlSBdpT4F5vb1frQ231', # The Other Side Of The Door
     '4pFvEWbjBpPUdYRQly0THs':'2JoJrsEV15OzbijS47lids', # Today Was A Fairytale
     # Wildest Dreams
-    '59HjlYCeBsxdI0fcm3zglw':'1Ov37jtRQ2YNAe8HzfczkL'
+    '59HjlYCeBsxdI0fcm3zglw':'1Ov37jtRQ2YNAe8HzfczkL',
+    # Red 1-16
+    '786NsUYn4GGUf8AOt0SQhP': '6lzc0Al0zfZOIFsFvBS1ki', # State of Grace
+    '0cITLOYn1Sv4q27zZPqlNK': '4OAuvHryIVv4kMDNSLuPt6', # Red
+    '5kYFVSQoPu7yRpfiHBwMUk': '3S7HNKPakdwNEBFIVTL6dZ', # Treacherous
+    '72jCZdH0Lhg93z6Z4hBjgj': '6AtZLIzUINvExIUy4QhdjP', # I Knew You Were Trouble
+    '4XMP3zVxrnr58T0tjIHvpR': '3nsfB1vus2qaloUdcBZvDu', # All Too Well
+    '2ULNeSomDxVNmdDy8VxEBU': '3yII7UwgLF6K5zW3xad3MP', # 22
+    '5VwFkx7JOimOGTYfha5rs1': '2r9CbjYgFhtAmcFv1cSquB', # I Almost Do
+    '7AEAGTc8cReDqcbPoY9gwo': '5YqltLsjdqFtvqE7Nrysvs', # We Are Never Ever Getting Back Together
+    '1qLeEu4iXEclkLwoBlMiou': '7eQj6r5PIdYKEIZjucBMcq', # Stay Stay Stay
+    '3n9mzjgAzjyKawvbHzciXF': '0y6kdSRCVQhSsHSpWvTUm7', # The Last Time
+    '1fGsZsIkQkOspfsT24nQP6': '7J4b3LVCIGO4CMBDFLPoP6', # Holy Ground
+    '6LKjHhOW1az75pCQ9XJJtF': '73qMN9bXy7MSPwwGfH3wQr', # Sad Beautiful Tragic
+    '2X2J0BhxaLTmnxO4pPUhSd': '4e5ayHsOLJNLTGfjau2mEw', # The Lucky One
+    '4NAmRvqSITAAzKWnC8yRq3': '7qEUFOVcxRI19tbT68JcYK', # Everything Has Changed
+    '7MecVsMj22MneZt7kVFaCr': '7A2cNLRT0YJc1yjxHlKihs', # Starlight
+    '0L4YCNRfXAoTvdpWeH2RGj': '05GsNucq8Bngd9fnd4fRa0', # Begin Again
+    # Red Deluxe
+    '5AsmAjBSSQjRPWgAYIp8tm': '0NRHj8hDwwmSPaA41o379r', # The Moment I Knew
+    '4qyalTRMRH5YuEntvvMvq0': '4pNApnaUWAL2J4KO2eqokq', # Come Back...Be Here
+    '6cf6rLb8qcklvJv90W6HCW': '0DMVrlMUn01M0IcpDbwgu7', # Girl At Home
+    '5G9AVKld9q7DCrmoY42raf': '5jAIouBES8LWMiriuNq170', # State Of Grace (Acoustic)
+    # Ronan
+    '0Nw8hv79MLJa1yjtsEgz08': '7nWui6jiMM2m9qFmET1Mtj'
 }
+
 
 def fetch_tv_id(id):
     if id in taylors_version_equivalents.keys():
